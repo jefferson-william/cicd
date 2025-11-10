461d4668c69c25794133b8482aac89b053f0b225
Jefferson Machado <jefferson.machado@superfrete.com>
2025-11-10 19:22:21 -0300

    feat: start hono server

---
+++ index.ts
@@ -0,0 +1,6 @@
+import app from './src/server'
+
+export default {
+  fetch: app.fetch,
+  port: 3000,
+}
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', c => c.text('🏠 Hello from Hono + Bun!'))
+app.get('/about', c => c.text('ℹ️ About route'))
+app.get('/api/json', c => c.json({ message: 'Hello JSON!' }))
+
+export default app
