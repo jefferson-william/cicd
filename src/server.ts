a1f5b3ea872714a7bb799c4ec6f77dfc706802fe
Jefferson Machado <jefferson.machado@superfrete.com>
2025-11-10 21:04:40 -0300

    feat: fix bun check

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => c.text('🏠 Hello from Hono + Bun!'))
-app.get('/about', (c) => c.text('ℹ️ About route'))
-app.get('/api/json', (c) => c.json({ message: 'Hello JSON!' }))
+app.get('/', c => c.text('🏠 Hello from Hono + Bun!'))
+app.get('/about', c => c.text('ℹ️ About route'))
+app.get('/api/json', c => c.json({ message: 'Hello JSON!' }))
 
 export default app
--- sst.config.ts
+++ sst.config.ts
@@ -1,36 +1,36 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-	app() {
-		return {
-			home: 'aws',
-			name: 'cicd',
-			protect: false,
-			providers: {
-				aws: {
-					profile: 'cicd',
-					region: 'us-east-1',
-				},
-			},
-			removal: 'remove',
-		}
-	},
-	async run() {
-		const vpc = new sst.aws.Vpc('Vpc', {
-			bastion: true,
-			nat: 'ec2',
-		})
-		const cluster = new sst.aws.Cluster('Cluster', {
-			vpc,
-		})
-		new sst.aws.Service('Server', {
-			cluster,
-			dev: {
-				command: 'bun dev',
-			},
-			loadBalancer: {
-				ports: [{ forward: '3000/http', listen: '80/http' }],
-			},
-		})
-	},
+  app() {
+    return {
+      home: 'aws',
+      name: 'cicd',
+      protect: false,
+      providers: {
+        aws: {
+          profile: 'cicd',
+          region: 'us-east-1',
+        },
+      },
+      removal: 'remove',
+    }
+  },
+  async run() {
+    const vpc = new sst.aws.Vpc('Vpc', {
+      bastion: true,
+      nat: 'ec2',
+    })
+    const cluster = new sst.aws.Cluster('Cluster', {
+      vpc,
+    })
+    new sst.aws.Service('Server', {
+      cluster,
+      dev: {
+        command: 'bun dev',
+      },
+      loadBalancer: {
+        ports: [{ forward: '3000/http', listen: '80/http' }],
+      },
+    })
+  },
 })
