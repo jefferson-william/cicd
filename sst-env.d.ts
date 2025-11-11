ef9b0f2a7c1003b5ca52ce54a824bd278b2e27db
Jefferson Machado <jefferson.machado@superfrete.com>
2025-11-11 00:32:37 -0300

    feat: sst remove (destroy)

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -3,19 +3,11 @@
 /* eslint-disable */
 /* deno-fmt-ignore-file */
 
-declare module 'sst' {
+declare module "sst" {
   export interface Resource {
-    Server: {
-      service: string
-      type: 'sst.aws.Service'
-      url: string
-    }
-    Vpc: {
-      bastion: string
-      type: 'sst.aws.Vpc'
-    }
   }
 }
 /// <reference path="sst-env.d.ts" />
 
-import 'sst'
+import "sst"
+export {}
